a9f30db743194cec9663074be5fa76fe23916505
Kamil Kwiatkowski <kamil.kwiatkowski2684@gmail.com>
2023-06-01 01:28:16 +0200

    added navbar and top panel

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import HomeNavigator from "./navigatior/Navigator";
 
-
 export default function App() {
     return (
         <NavigationContainer>
--- screens/Home.js
+++ screens/Home.js
@@ -3,6 +3,9 @@ import { useCallback } from "react";
 import { StyleSheet, Text, View, Pressable, StatusBar, SafeAreaView, Image, ToastAndroid, Dimensions } from "react-native";
 import { useFonts, OpenSans_400Regular, OpenSans_300Light, OpenSans_700Bold } from "@expo-google-fonts/open-sans";
 import * as SplashScreen from "expo-splash-screen";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faPlus, faBars, faLeaf} from "@fortawesome/free-solid-svg-icons/";
+
 
 const w = Dimensions.get("window").width;
 
@@ -27,62 +30,189 @@ const Home = () => {
     return (
         <SafeAreaView style={{ flex: 1 }}>
             <View style={styles.container} onLayout={onLayoutRootView}>
-                <StatusBar backgroundColor={"#dad7cd"} />
-                <UserPanel />
+                <StatusBar backgroundColor={colors.darkGreen} />
+                <TopPanel />
+                <NavBar />
             </View>
         </SafeAreaView>
-        
     );
 };
 
 export default Home;
 
-const UserPanel = () => {
+const TopPanel = () => {
+    return (
+        <View style={styles.topPanel}>
+            <UserPressable />
+            <BurgerIcon />
+        </View>
+    );
+};
+
+const UserPressable = () => {
     const onPress = () => {
-        ToastAndroid.show("Kliknięto przycisk!", ToastAndroid.SHORT);
+        ToastAndroid.show("Kliknięto panel użytkownika!", ToastAndroid.SHORT);
     };
 
     return (
-        <Pressable style={styles.userPanel} onLongPress={onPress}>
+        <Pressable style={styles.userPressable} onPress={onPress}>
             <Image source={require("../assets/prof.jpg")} style={styles.img}></Image>
-            <Text style={[styles.userName, styles.font]}>Kamil Kwiatkowski</Text>
+            <View>
+                <Text style={[styles.fontN, styles.greeting]}>Witaj</Text>
+                <Text style={[styles.userName, styles.fontB]}>Kamil Kwiatkowski</Text>
+            </View>
         </Pressable>
     );
 };
 
+const BurgerIcon = () => {
+    const onPress = () => {
+        ToastAndroid.show("Kliknięto burger ikonke!", ToastAndroid.SHORT);
+    };
+
+    return (
+        <Pressable style={styles.burgerIcon} onPress={onPress}>
+            <FontAwesomeIcon icon={faBars} size={40} style={{ marginRight: 30 }} color={colors.main} />
+        </Pressable>
+    );
+};
+
+const NavBar = () => {
+    return (
+        <View style={styles.navBar}>
+            <View style={styles.btnView}>
+                <InfoBtn />
+            </View>
+            <View style={styles.btnView}>
+                <PlusBtn />
+                <Text style={[styles.btnCaption, styles.fontB]}>Dodaj</Text>
+            </View>
+            <View style={styles.btnView}>
+                <Btn />
+
+            </View>
+        </View>
+    );
+};
+
+const PlusBtn = () => {
+    const onPress = () => {
+        ToastAndroid.show("Kliknięto plus ikonke!", ToastAndroid.SHORT);
+    };
+
+    return (
+        <Pressable style={styles.plusBtn} onPress={onPress}>
+            <FontAwesomeIcon icon={faPlus} size={40} color={colors.yellow} />
+        </Pressable>
+    );
+};
+
+const InfoBtn = () => {
+    const onPress = () => {
+        ToastAndroid.show("Kliknięto info ikonke!", ToastAndroid.SHORT);
+    };
+
+    return (
+        <Pressable style={styles} onPress={onPress}>
+            <FontAwesomeIcon icon={faPlus} size={40} color={colors.darkGreen} />
+        </Pressable>
+    );
+};
+const Btn = () => {
+    const onPress = () => {
+        ToastAndroid.show("Kliknięto info ikonke!", ToastAndroid.SHORT);
+    };
+
+    return (
+        <Pressable style={styles} onPress={onPress}>
+            <FontAwesomeIcon icon={faLeaf} size={40} color={colors.darkGreen} />
+        </Pressable>
+    );
+};
+
+const colors = {
+    main: "#f1f7ed",
+    lightGreen: "#e0eec6",
+    darkGreen: "#243e36",
+    green: "#7ca982",
+    yellow: "#c2a83e",
+    navBar: "#dad7cd",
+};
+
 const styles = StyleSheet.create({
-    font: {
+    fontL: {
+        fontFamily: "OpenSans_300Light",
+    },
+    fontN: {
+        fontFamily: "OpenSans_400Regular",
+    },
+    fontB: {
         fontFamily: "OpenSans_700Bold",
     },
     container: {
         flex: 1,
-        backgroundColor: "#344e41",
+        backgroundColor: colors.main,
     },
-    header: {
+    topPanel: {
         flexDirection: "row",
         alignItems: "center",
-    },
-    userPanel: {
-        flexDirection: "row",
-        alignItems: "center",
+        justifyContent: "space-between",
         width: w,
-        height: 160,
+        height: "15%",
+        backgroundColor: colors.darkGreen,
         borderBottomEndRadius: 20,
         borderBottomStartRadius: 20,
-        overflow: "hidden",
-        backgroundColor: "#dad7cd",
+    },
+    userPressable: {
+        width: "70%",
+        flexDirection: "row",
+        alignItems: "center",
+    },
+    greeting: {
+        color: colors.main,
     },
     userName: {
-        color: "#0b100d",
+        color: colors.lightGreen,
         fontSize: 18,
     },
     img: {
-        marginHorizontal: 10,
+        borderWidth: 3,
+        borderColor: colors.lightGreen,
+        marginLeft: 20,
+        marginRight: 15,
         width: 55,
         height: 55,
+        borderRadius: 55,
+    },
+
+    navBar: {
+        position: "absolute",
+        bottom: 0,
+        right: 0,
+        left: 0,
+        flexDirection: "row",
+        justifyContent: "space-around",
+
+        margin: 20,
+        borderRadius: 20,
+        height: 110,
+        backgroundColor: colors.navBar,
+    },
+    plusBtn: {
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: colors.darkGreen,
+        width: 60,
+        height: 60,
         borderRadius: 50,
     },
-    text: {
-        color: "black",
+    btnCaption: {
+        textTransform: "uppercase",
+        color: colors.darkGreen,
+    },
+    btnView: {
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "space-evenly",
     },
 });
